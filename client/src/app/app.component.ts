a69b5aea8d5d155971515379e66f4511a3c3b5c0
arinleviti <arin.leviti@gmail.com>
2025-02-21 15:38:39 +0100

    Register + home page
    Insertion of register form and a home page.

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,23 +1,20 @@
 import { Component,OnInit, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {HttpClient} from '@angular/common/http';
 import { NavComponent } from "./nav/nav.component";
 import { AccountService } from './_services/account.service';
+import { HomeComponent } from "./home/home.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavComponent],
+  imports: [RouterOutlet, NavComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent implements OnInit{
-  http = inject(HttpClient);
-  title = 'Dating App';
-  users: any;
+
   private accountService = inject (AccountService);
 
   ngOnInit(): void {
-    this.getUsers();
     this.setCurrentUser();
   }
 
@@ -26,14 +23,5 @@ export class AppComponent implements OnInit{
     if (!userString) return;
     const user = JSON.parse(userString);
     this.accountService.currentUser.set(user);
-  }
-  
-
-  getUsers() {
-    this.http.get('https://localhost:5001/api/users').subscribe({
-      next: response => this.users = response,
-      error: error => console.log(error),
-      complete: () => console.log('complete') 
-  });
   }
 }
